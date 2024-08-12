e2548a3f083ae8e5fe0819252c61c8eabe808167
Klevisa Kolaj <klevisa.kolaj1@gmail.com>
2024-08-12 11:13:56 -0400

    the very first commit

---
+++ src/test/java/basicCode.java
@@ -0,0 +1,18 @@
+public class basicCode {
+    public static void main(String[] args) {
+
+        System.out.println("hello B17");
+    }
+}
+
+/*
+
+1.pwd (shiko nqs esht titulli njesoj)
+2.git init (initialize a repository
+3.     (add everything to staging area)
+
+
+
+
+
+ */
